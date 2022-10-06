1ce38812ae428cc1712afd6da74238c742a04239
Oqilbek2121 <oqilbek851@gmail.com>
2022-10-06 21:40:50 +0500

    Scroll figma

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal');
+
+    for(let i = 0; i < reveals.length; i++) {
+        let windowheight = window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        let revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+let loader = document.getElementById('preloader');
+
+      window.addEventListener('load', function () {
+          loader.style.display = 'none';
+      })
